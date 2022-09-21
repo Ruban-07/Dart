77944c9a826bdccd7a1120d397dafbe71178bf25
Ruban-07 <ruban@yavar.in>
2022-09-21 18:50:07 +0530

    Readme file

---
+++ programFive.dart
@@ -0,0 +1,16 @@
+// import 'dart:io';
+
+// void main() {
+//   stdout.write("Enter a number : ");
+
+//   int userInput = int.parse(stdin.readLineSync()!);
+
+//   for (int i = 0; i <= userInput; i++) {
+//     int divisors = i % userInput;
+//     if (divisors == 0) {
+//       print("ruban");
+//       break;
+//     }
+//     print(divisors);
+//   }
+// }
